6c2575b8952b7b9f84188df5bafa4073618732b1
Ugljesa Mitrovic <umitrovic@bettercollective.local>
2022-10-07 14:58:10 +0200

    Initial commit

---
+++ ChangeGuidsTextTool/Program.cs
@@ -0,0 +1,21 @@
+﻿using ChangeGuidsTextTool;
+using MiscUtil.IO;
+using System.Text.RegularExpressions;
+
+//bin/Debug/Net6.0
+//Copy whole book or just tasks you need into book.txt 
+//Copy more than you need newly generated guids in guids.txt
+//Start
+//newBook <- whole book/tasks wtih new guids and sportsbook name
+//newGuidsForScript <- guids in format easy to copy into script
+//guidsForScript <- in case you just need guids for script from tasks that don't need change
+
+Services service = new Services();
+service.makeNewBookAndScript();
+service.extractGuids();
+
+
+
+
+
+
---
+++ ChangeGuidsTextTool/Services.cs
@@ -0,0 +1,73 @@
+﻿using MiscUtil.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ChangeGuidsTextTool
+{
+    public class Services
+    {
+        private string book;
+        private string newBook;
+        private List<string> guidList;
+        private static string guidPattern = "[a-z0-9]{32}";
+        private static string sportBookPattern = "(SportsbookEnum\\.[a-zA-z]+)";
+
+        public Services()
+        {
+            book = File.ReadAllText("book.txt");
+            guidList = new List<string>();
+            foreach (string line in new LineReader(() => new StringReader(File.ReadAllText("guids.txt"))))
+            {
+                guidList.Add(line);
+            }
+        }
+
+        public void makeNewBookAndScript()
+        {
+            Console.WriteLine("Enter new sportsbook name: \n");
+            var sportsBookName = Console.ReadLine();
+            int i = 0;
+            string newGuidsForScript = "";
+
+            foreach (string line in new LineReader(() => new StringReader(book)))
+            {
+                string input = line;
+
+                string sportBookReplacement = "SportsbookEnum." + sportsBookName;
+
+                string guidReplacement = guidList[i];
+                string guidResultLine = Regex.Replace(input, guidPattern, guidReplacement);
+
+                if (guidResultLine != input)
+                {
+                    newGuidsForScript += "\'" + guidList[i] + "\'," + "\n";
+                    i++;
+                }
+                newBook += Regex.Replace(guidResultLine, sportBookPattern, sportBookReplacement) + "\n";
+            }
+            File.WriteAllText("newBook.txt", newBook);
+            File.WriteAllText("newGuidsForScript.txt", newGuidsForScript);
+        }
+
+        //In case you have finished book but need guids for script
+        public void extractGuids()
+        {
+            var guids = "";
+            foreach (string line in new LineReader(() => new StringReader(book)))
+            {
+                string input = line;
+                Match m = Regex.Match(input, guidPattern, RegexOptions.None);
+                if (m.Success)
+                    guids += "\'" + m.Value + "\'," + "\n";
+                File.WriteAllText("guidsForScript.txt", guids);
+            }
+
+
+        }
+    }
+ 
+}
